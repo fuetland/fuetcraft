9aa1f2a6fc9f1dcdd8c17a8a53c8c97cc67f6d78
Victor Monzo Mora <49898335+VictorMonzo@users.noreply.github.com>
2025-04-28 10:43:39 +0200

    fix: Se cambian las imagenes de cada fuet

--- src/main/java/fuetcraft/item/FuetTarradellasItem.java
+++ src/main/java/fuetcraft/item/FuetTarradellasItem.java
@@ -32,7 +32,7 @@ public class FuetTarradellasItem extends Item {
 		if (equipmentSlot == EquipmentSlot.MAINHAND) {
 			ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
 			builder.putAll(super.getDefaultAttributeModifiers(equipmentSlot));
-			builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Item modifier", 5d, AttributeModifier.Operation.ADDITION));
+			builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Item modifier", 7d, AttributeModifier.Operation.ADDITION));
 			builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Item modifier", -2.4, AttributeModifier.Operation.ADDITION));
 			return builder.build();
 		}
