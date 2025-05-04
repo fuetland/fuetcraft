797b48fbe55f41e99b387ee8e17e8bd19dda1a05
Victor Monzo Mora <49898335+VictorMonzo@users.noreply.github.com>
2025-05-04 12:21:10 +0200

    feat: Se añaden las recetas de Stuffer y Chopper al libro de recetas

--- src/main/java/fuetcraft/init/FuetcraftModTabs.java
+++ src/main/java/fuetcraft/init/FuetcraftModTabs.java
@@ -18,21 +18,21 @@ public class FuetcraftModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FuetcraftMod.MODID);
 	public static final RegistryObject<CreativeModeTab> FUETCRAFT = REGISTRY.register("fuetcraft",
 			() -> CreativeModeTab.builder().title(Component.translatable("item_group.fuetcraft.fuetcraft")).icon(() -> new ItemStack(FuetcraftModItems.FUET.get())).displayItems((parameters, tabData) -> {
-				tabData.accept(FuetcraftModBlocks.CHOPPER.get().asItem());
 				tabData.accept(FuetcraftModItems.PORKCHOP_CHOPPED.get());
 				tabData.accept(FuetcraftModItems.MINING_PORKCHOP_CHOPPED.get());
 				tabData.accept(FuetcraftModItems.EXPLORER_PORKCHOP_CHOPPED.get());
-				tabData.accept(FuetcraftModItems.EXPIRED_PORKCHOP_CHOPPED.get());
 				tabData.accept(FuetcraftModItems.SWIMMING_PORKCHOP_CHOPPED.get());
 				tabData.accept(FuetcraftModItems.COMBAT_PORKCHOP_CHOPPED.get());
+				tabData.accept(FuetcraftModItems.EXPIRED_PORKCHOP_CHOPPED.get());
 				tabData.accept(FuetcraftModItems.TARRADELLAS_PORKCHOP_CHOPPED.get());
 				tabData.accept(FuetcraftModItems.FUET.get());
-				tabData.accept(FuetcraftModBlocks.STUFFER.get().asItem());
+				tabData.accept(FuetcraftModItems.FUET_MINING.get());
+				tabData.accept(FuetcraftModItems.FUET_EXPLORING.get());
+				tabData.accept(FuetcraftModItems.FUET_SWIMMING.get());
 				tabData.accept(FuetcraftModItems.FUET_COMBAT.get());
 				tabData.accept(FuetcraftModItems.FUET_EXPIRED.get());
-				tabData.accept(FuetcraftModItems.FUET_EXPLORING.get());
-				tabData.accept(FuetcraftModItems.FUET_MINING.get());
-				tabData.accept(FuetcraftModItems.FUET_SWIMMING.get());
 				tabData.accept(FuetcraftModItems.FUET_TARRADELLAS.get());
+				tabData.accept(FuetcraftModBlocks.CHOPPER.get().asItem());
+				tabData.accept(FuetcraftModBlocks.STUFFER.get().asItem());
 			}).withSearchBar().build());
 }
---
+++ src/main/java/fuetcraft/procedures/GiveChopperRecipeProcedure.java
@@ -0,0 +1,52 @@
+package fuetcraft.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.player.LocalPlayer;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class GiveChopperRecipeProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player);
+		}
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (!(new Object() {
+			public boolean hasRecipe(Entity _ent, ResourceLocation recipe) {
+				if (_ent instanceof ServerPlayer _player)
+					return _player.getRecipeBook().contains(recipe);
+				else if (_ent.level().isClientSide() && _ent instanceof LocalPlayer _player)
+					return _player.getRecipeBook().contains(recipe);
+				return false;
+			}
+		}.hasRecipe(entity, new ResourceLocation("fuetcraft:chopper_recipe"))) && ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(Items.IRON_INGOT)) : false)
+				|| (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(Items.IRON_SWORD)) : false)
+				|| (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(Blocks.REDSTONE_BLOCK)) : false)
+				|| (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(Blocks.HOPPER)) : false)
+				|| (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(Blocks.DISPENSER)) : false))) {
+			if (entity instanceof ServerPlayer _serverPlayer)
+				_serverPlayer.awardRecipesByKey(new ResourceLocation[]{new ResourceLocation("fuetcraft:chopper_recipe")});
+		}
+	}
+}
